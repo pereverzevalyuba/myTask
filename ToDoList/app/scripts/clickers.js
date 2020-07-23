3e509c86fea227c3ee33d635a180eae3a3f750cc
Lyuba <pereverxeva@gmail.com>
2020-07-23 16:36:02 +0300

    Enitial commit

---
+++ ToDoList/app/scripts/clickers.js
@@ -0,0 +1,70 @@
+var input = document.querySelector("input[type='text']");
+var ul = document.querySelector("ul");
+var pencil = document.querySelector("#pencil");
+var saveBtn = document.querySelector(".save");
+var clearBtn = document.querySelector(".clear");
+var tipsBtn = document.querySelector(".tipBtn");
+var closeBtn = document.querySelector(".closebtn");
+var overlay = document.getElementById("overlay");
+
+export function keypress(callback) {
+  // Add todo element to todo list
+  input.addEventListener("keypress", function (key) {
+    if (key.which === 13) {
+      var li = document.createElement("li");
+      var spanElement = document.createElement("span");
+      var icon = document.createElement("i");
+
+      var newTodo = this.value;
+      this.value = "";
+
+      icon.classList.add("fas", "fa-trash-alt");
+      console.log(icon.classList);
+      spanElement.append(icon);
+      ul.appendChild(li).append(spanElement, newTodo);
+
+      callback();
+    }
+  });
+}
+
+export const ul_checked = () => {
+  ul.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    }
+  });
+};
+
+export const pencil_show_hide = () => {
+  pencil.addEventListener("click", function () {
+    input.classList.toggle("display");
+  });
+};
+
+export const tips = () => {
+  tipsBtn.addEventListener("click", function () {
+    overlay.style.height = "100%";
+  });
+};
+
+export const close = () => {
+  closeBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    overlay.style.height = "0";
+  });
+};
+
+export const clear_todo = () => {
+  // Clear all todos on click Clear button
+  clearBtn.addEventListener("click", function () {
+    ul.innerHTML = "";
+  });
+};
+
+export const save_todo = () => {
+  saveBtn.addEventListener("click", function () {
+    localStorage.setItem("todoList", ul.innerHTML);
+    console.log(localStorage);
+  });
+};
---
+++ ToDoList/app/scripts/main.js
@@ -0,0 +1,54 @@
+import { keypress, ul_checked, pencil_show_hide, save_todo, clear_todo, tips, close } from "./clickers.js";
+import { cat_facts } from "./cat_facts.js";
+
+// Document ready
+$(document).ready(() => {
+  var spans = document.getElementsByTagName("span");
+  var ul = document.querySelector("ul");
+  var ul_cat_facts = document.querySelector('.cat-facts');
+
+  // Initialize event listeners
+  keypress(deleteTodo);
+  ul_checked();
+  pencil_show_hide();
+  save_todo();
+  clear_todo();
+  tips();
+  close();
+
+  // Load cat facts data
+  cat_facts("https://cat-fact.herokuapp.com/facts/random?animal_type&amount=10")
+    .done(data => {
+      console.log(data);
+      setFactsData(data);
+    })
+    .fail(err => {
+      console.log(err);
+    })
+
+    function setFactsData(cat_facts) {
+      cat_facts.map(elem => {
+        ul_cat_facts.append(document.createElement('li'), elem.text)
+      })
+      console.log(ul_cat_facts);
+    }
+
+  // Delete todo if delete span clicked
+  function deleteTodo() {
+    for (let span of spans) {
+      span.addEventListener("click", function () {
+        console.log(span.parentElement);
+        span.parentElement.remove();
+      });
+    }
+  }
+
+  function loadTodos() {
+    if (localStorage.getItem("todoList")) {
+      ul.innerHTML = localStorage.getItem("todoList");
+    }
+  }
+
+  deleteTodo();
+  loadTodos();
+});
